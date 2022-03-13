1bf89980c710864e2b9e2f6b6ba5475e2bb75397
Selvarajan Thangavel <selvarajan.thangavel@d4t4solutions.com>
2022-03-13 12:39:51 +0530

    Initial Commit

---
+++ Mozhi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mozhi
+//
+//  Created by Selvarajan on 13/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mozhi/MozhiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MozhiApp.swift
+//  Mozhi
+//
+//  Created by Selvarajan on 13/03/22.
+//
+
+import SwiftUI
+
+@main
+struct MozhiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
